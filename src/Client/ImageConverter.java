64028c92b1a9a533636b5caf709cbc2c2daf0ee9
doding2 <65176422+doding2@users.noreply.github.com>
2020-12-16 20:10:07 +0900

    Upload files

---
+++ src/Client/ImageConverter.java
@@ -0,0 +1,50 @@
+package Client;
+
+
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
+
+import javax.swing.ImageIcon;
+
+public class ImageConverter {
+
+	private static Dimension arcs = new Dimension(65, 65);
+
+	public static ImageIcon rounding(ImageIcon original, Dimension size) {
+
+		int w = size.width;
+		int h = size.height;
+		Image t = original.getImage().getScaledInstance(w, h, java.awt.Image.SCALE_SMOOTH);
+		ImageIcon sized = new ImageIcon(t);
+		BufferedImage output = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+
+		Graphics2D g2 = output.createGraphics();
+
+		g2.setComposite(AlphaComposite.Src);
+		
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.setColor(Color.WHITE);
+		g2.fill(new RoundRectangle2D.Float(0, 0, w, h, arcs.width, arcs.height));
+
+		g2.setComposite(AlphaComposite.SrcAtop);
+		g2.drawImage(sized.getImage(), 0, 0, null);
+
+		g2.dispose();
+
+		return new ImageIcon(output);
+	}
+	
+	// 이미지 사이즈 크기 조절
+	public static BufferedImage scale(BufferedImage img, int newW, int newH) {
+		 Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+		    BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+
+		    Graphics2D g2d = dimg.createGraphics();
+		    g2d.drawImage(tmp, 0, 0, null);
+		    g2d.dispose();
+
+		    return dimg;
+	}
+
+}
